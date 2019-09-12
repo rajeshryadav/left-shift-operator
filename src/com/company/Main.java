3578902f34c06f5ca0e5a4a0127563b38c9c2343
Rahul kumar <rahulkumar96369@gmail.com>
2019-09-12 20:37:11 +0530

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(10<<2);
+        System.out.println(10<<3);
+        System.out.println(20<<2);
+        System.out.println(15<<4);
+        System.out.println(100<<2);
+    }
+}
